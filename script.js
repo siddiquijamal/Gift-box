9ba3758defadff02de244e3b5a885e2f82f716c2
siddiquijamal <161710582+siddiquijamal@users.noreply.github.com>
2025-02-08 09:43:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function changeGift() {
 	const giftDiv = document.getElementById("gift");
 	const gifts = {
-	  1: { emoji: "🌹", text: "It's Rose Day" },
-	  2: { emoji: "❤️", text: "It's Propose Day" },
-	  3: { emoji: "🍫", text: "It's Chocolate Day" },
-	  4: { emoji: "🧸", text: "It's Teddy Day" },
-	  5: { emoji: "🤝", text: "It's Promise Day" },
-	  6: { emoji: "🤗", text: "It's Hug Day" },
-	  7: { emoji: "💋", text: "It's Kiss Day" },
-	  8: { emoji: "💘", text: "It's Valentine’s Day"},
+	  1: { emoji: "🌹", text:"7 Feb". "It's Rose Day" },
+	  2: { emoji: "❤️", text:"8 Feb". "It's Propose Day" },
+	  3: { emoji: "🍫", text:"9 Feb". "It's Chocolate Day" },
+	  4: { emoji: "🧸", text:"10 Feb". "It's Teddy Day" },
+	  5: { emoji: "🤝", text:"11 Feb". "It's Promise Day" },
+	  6: { emoji: "🤗", text:"12 Feb". "It's Hug Day" },
+	  7: { emoji: "💋", text: "13 Feb"."It's Kiss Day" },
+	  8: { emoji: "💘", text: "14 Feb". "It's Valentine’s Day"},
 	};
   
 	const days = Object.keys(gifts);
